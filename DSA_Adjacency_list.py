3779066276bc6c13f418f1aa88475245ba669982
Tripu07 <87227815+Tripu07@users.noreply.github.com>
2021-10-06 16:25:25 +0530

    Add files via upload

--- DSA_Adjacency_list.py
+++ DSA_Adjacency_list.py
@@ -19,6 +19,25 @@ def insert_edge(a,b,weight):    #INSERTION OF EDGE BETWEEN TWO GIVEN NODES  ,wth
         temp2=nodes.index(b)
         a1[temp1].append((b,weight))
         a1[temp2].append((a,weight))
+def delete_node(data):
+    if data in nodes:
+        nodes.remove(data)
+        graph.pop(data)
+        for i in a1:
+            for j in i:
+                for k in j:
+                    if k==data:
+                        i.remove(j)
+def delete_edge(a,b,weight):
+    if a and b in nodes:
+        temp1=nodes.index(a)
+        temp2=nodes.index(b)
+        a1[temp1].remove((b,weight))
+        a1[temp2].remove((a,weight))
+        
+
+        
+            
 insert_node("A")
 insert_node("B")
 insert_node("C")
@@ -27,6 +46,8 @@ insert_edge("A","B",5)
 insert_edge("C","D",8)
 insert_edge("A","C",9)
 insert_edge("B","D",2)
+delete_node("C")
+delete_edge("A","B",5)
 print(nodes)
 print_graph()
     
